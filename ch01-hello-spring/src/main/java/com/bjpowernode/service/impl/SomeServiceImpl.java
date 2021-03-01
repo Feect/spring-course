22c8a589d4d7591002fa70d8d0f8efef91c138cc
Feect <1076347824@qq.com>
2021-03-01 15:26:38 +0800

    第一章后半段

--- ch01-hello-spring/src/main/java/com/bjpowernode/service/impl/SomeServiceImpl.java
+++ ch01-hello-spring/src/main/java/com/bjpowernode/service/impl/SomeServiceImpl.java
@@ -3,6 +3,11 @@ package com.bjpowernode.service.impl;
 import com.bjpowernode.service.SomeService;
 
 public class SomeServiceImpl implements SomeService {
+
+    public SomeServiceImpl() {
+        System.out.println("执行了SomeServiceImpl()的无参构造方法");
+    }
+
     @Override
     public void doSome() {
         System.out.println("执行了SomeServiceImpl的doSome()方法");
--- ch01-hello-spring/src/test/java/com/bjpowernode/MyTest.java
+++ ch01-hello-spring/src/test/java/com/bjpowernode/MyTest.java
@@ -19,11 +19,41 @@ public class MyTest
         service.doSome();
     }
 
+
+    /**
+     * spring默认创建对象的时间：在创建spring的容器时，会创建配置文件中的所有对象
+     */
     @Test
     public void test02(){
+        //使用spring容器创建对象
+        //1.指定spring配置文件的名称
         String config = "beans.xml";
+        //2.创建表示spring容器的对象，ApplicationContext
+        //ApplicationContext就是表示spring容器，通过容器获取对象
+        //ClassPathXmlApplicationContext：表示从类路径中加载spring的配置文件
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(config);
+        //从容器中获取某个对象，你要调用对象的方法
+        //getBean("配置文件中的bean的id值")
         SomeService service = (SomeService) applicationContext.getBean("someService");
         service.doSome();
+    }
+
+
+    /**
+     * 获取spring容器中java对象的数量
+     */
+    @Test
+    public void test03(){
+        String config = "beans.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(config);
+        //使用spring提供的方法，获取容器中定义的对象的数量
+        int nums = applicationContext.getBeanDefinitionCount();
+        System.out.println("容器中定义的对象的数量位：" + nums);
+
+        //容器中每个定义的对象的名称
+        String names[] = applicationContext.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
